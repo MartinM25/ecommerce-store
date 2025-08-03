8c544f0b3b1effed4d653b7140d0743f99e63f93
Martin Manjoro <martin.manjoro25@gmail.com>
2025-08-03 21:24:41 +0200

    Update next.config.ts
    -Configuration of next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {},
+  },
 };
 
 export default nextConfig;
